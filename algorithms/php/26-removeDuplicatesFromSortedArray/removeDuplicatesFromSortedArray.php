5647effc63e614104a14866f88e6ce34d15f4764
uncleqiu <fq8563047@163.com>
2019-04-26 11:54:31 +0800

    leetcode problem 26 add new function

--- algorithms/php/26-removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.php
+++ algorithms/php/26-removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.php
@@ -25,4 +25,21 @@ class Solution {
         $nums = array_flip(array_flip($nums));
         return count($nums);
     }
+
+    /**
+     * Method four:
+     */
+    function removeDuplicates4(&$nums) {
+        $len = count($nums);
+        if ($len <= 1) {
+            return $len;
+        }
+        $pos = 0;
+        for ($i = 0; $i < ($len - 1); $i++) {
+            if ($nums[$i] != $nums[$i+1]) {
+                $nums[++$pos] = $nums[$i+1];
+            }
+        }
+        return $pos + 1;
+    }
 }
