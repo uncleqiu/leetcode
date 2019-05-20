40d1759028807a423448b5c34cd1a1ea8886150d
uncleqiu <fq8563047@163.com>
2019-05-20 11:21:00 +0800

    Leetcode problem 9 add another solution

--- algorithms/php/9-palindromeNumber/palindromNumber.php
+++ algorithms/php/9-palindromeNumber/palindromNumber.php
@@ -1,6 +1,26 @@
 <?php
     
+    /**
+     * Method one:
+     * using php function
+     */
     function isPalindrome($x) {
         $str = (string)$x;
         return strrev($str) == $str;
+    }
+
+    /**
+     * Method two:
+     */
+    function isPalindrome2($x) {
+        if ($x < 0) {
+            return false;
+        }
+        $x = (string)$x;
+        $len = strlen($x);
+        $str = '';
+        for ($i = 0; $i < $len; $i++) {
+            $str = $x[$i] . $str;
+        }
+        return $x == $str;
     }
