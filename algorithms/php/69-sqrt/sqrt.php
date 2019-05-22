c3fe921a23d95ee0650c31ef40322b06b3c8f492
uncleqiu <fq8563047@163.com>
2019-05-22 15:11:04 +0800

    add binary search method to solve leetcode problem 69 and update README

--- algorithms/php/69-sqrt/sqrt.php
+++ algorithms/php/69-sqrt/sqrt.php
@@ -9,4 +9,25 @@ class Solution {
     function mySqrt($x) {
         return intval(sqrt($x));
     }
+
+    /**
+     * Method two:
+     * binary search
+     */
+    function mySqrt2($x) {
+        $head = 1;
+        $tail = $x / 2 + 1;
+        while ($head <= $tail) {
+            $mid = intval(($head + $tail) / 2);
+            $rs = $mid * $mid;
+            if ($x == $rs) {
+                return $mid;
+            } else if ($x > $rs) {
+                $head = $mid + 1;
+            } else {
+                $tail = $mid - 1;
+            }
+        }
+        return $tail;
+    }
 }
